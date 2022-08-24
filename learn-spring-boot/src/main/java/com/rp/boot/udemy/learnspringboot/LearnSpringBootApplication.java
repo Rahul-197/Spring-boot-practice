4f139627063798aa847dd05d131e176f630d35d2
Rahul-197 <rpatidar3101@gmail.com>
2022-08-24 11:22:22 +0530

    add responsebody with jsp html

--- learn-spring-boot/src/main/java/com/rp/boot/udemy/learnspringboot/LearnSpringBootApplication.java
+++ learn-spring-boot/src/main/java/com/rp/boot/udemy/learnspringboot/LearnSpringBootApplication.java
@@ -9,7 +9,7 @@ public class LearnSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringBootApplication.class, args);
 		System.out.println("***************spring boot application started***********");
-		System.out.println("***************jsm jsk jsr spring boot application started All***********");
+		System.out.println("***************jsm jsk jsr spring boot application started All************");
 
 	}
 
